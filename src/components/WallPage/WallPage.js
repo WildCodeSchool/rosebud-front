98dfd3494fa82262be24832a898c15030367e536
Falcao1992 <eduardo.marselle@gmail.com>
2019-12-04 23:10:57 +0100

    les images s'encastrent bien entre-elles

---
+++ src/components/WallPage/WallPage.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Participation from "./Participation/Participation";
+
+const WallPage = () => {
+  return (
+    <div className="wallpage__content">
+      <Participation />
+      <Participation />
+    </div>
+  );
+};
+
+export default WallPage;
