b2d83adc8688b2588216eb5ec16ecd7e000c54af
Falcao1992 <eduardo.marselle@gmail.com>
2019-12-05 14:59:55 +0100

    save before change heading

--- src/components/WallPage/Participation/Participation.js
+++ src/components/WallPage/Participation/Participation.js
@@ -3,165 +3,174 @@ import "./Participation.scss";
 
 const Participation = () => {
   return (
-    <div class="wrapper">
-      <header class="heading">Prénom - Nom - Ville</header>
-      <div class="item01">
-        <img
-          src="https://img4.cdn.cinoche.com/images/2545979f15444781bbd282e6e475feb0.jpg"
-          alt=""
-        />
-        <div class="meta">
-          <h2>Lorem ipsum dolor sit.</h2>
-          <ul class="cta-list">
-            <li class="cta-item">
-              <i class="material-icons">&#xE8DC;</i>
-            </li>
-            <li class="cta-item">
-              <i class="material-icons">&#xE0B9;</i>
-            </li>
-            <li class="cta-item">
-              <i class="material-icons">&#xE80D;</i>
-            </li>
-            <li class="cta-item">
-              <i class="material-icons">&#xE5D4;</i>
-            </li>
-          </ul>
+    <div className="participation__content">
+      <div className="participation__infos-personel">
+        <h1>Prénom Nom</h1>
+        <p>Ville</p>
+        <p>age</p>
+      </div>
+      <div class="wrapper__content">
+      <div class="wrapper">
+        <header class="heading"></header>
+        <div class="item01">
+          <img
+            src="https://img4.cdn.cinoche.com/images/2545979f15444781bbd282e6e475feb0.jpg"
+            alt=""
+          />
+          <div class="meta">
+            <h2>Lorem ipsum dolor sit.</h2>
+            <ul class="cta-list">
+              <li class="cta-item">
+                <i class="material-icons">&#xE8DC;</i>
+              </li>
+              <li class="cta-item">
+                <i class="material-icons">&#xE0B9;</i>
+              </li>
+              <li class="cta-item">
+                <i class="material-icons">&#xE80D;</i>
+              </li>
+              <li class="cta-item">
+                <i class="material-icons">&#xE5D4;</i>
+              </li>
+            </ul>
+          </div>
+        </div>
+        <div class="item02">
+          <img
+            src="https://tel.img.pmdstatic.net/fit/http.3A.2F.2Fprd2-bone-image.2Es3-website-eu-west-1.2Eamazonaws.2Ecom.2Ftel.2F2019.2F11.2F08.2F60ee5033-efd6-4c73-9403-a8fbaf4486cf.2Ejpeg/530x230/quality/80/slumdog-millionaire-arte-tournage-clandestin-casting-laborieux-la-genese-de-ce-film-phenomene.jpg"
+            alt=""
+          />
+          <div class="meta">
+            <h2>Lorem ipsum dolor sit.</h2>
+            <ul class="cta-list">
+              <li class="cta-item">
+                <i class="material-icons">&#xE8DC;</i>
+              </li>
+              <li class="cta-item">
+                <i class="material-icons">&#xE0B9;</i>
+              </li>
+              <li class="cta-item">
+                <i class="material-icons">&#xE80D;</i>
+              </li>
+              <li class="cta-item">
+                <i class="material-icons">&#xE5D4;</i>
+              </li>
+            </ul>
+          </div>
+        </div>
+        <div class="item03">
+          <img
+            src="https://upload.wikimedia.org/wikipedia/en/0/01/HarrietFilmPoster.jpeg"
+            alt=""
+          />
+          <div class="meta">
+            <h2>Lorem ipsum dolor sit.</h2>
+            <ul class="cta-list">
+              <li class="cta-item">
+                <i class="material-icons">&#xE8DC;</i>
+              </li>
+              <li class="cta-item">
+                <i class="material-icons">&#xE0B9;</i>
+              </li>
+              <li class="cta-item">
+                <i class="material-icons">&#xE80D;</i>
+              </li>
+              <li class="cta-item">
+                <i class="material-icons">&#xE5D4;</i>
+              </li>
+            </ul>
+          </div>
+        </div>
+        <div class="item04">
+          <img
+            src="https://www.lepoint.fr/images/2017/11/13/11277831lpw-11278045-article-jpg_4757541.jpg"
+            alt=""
+          />
+          <div class="meta">
+            <h2>Lorem ipsum dolor sit.</h2>
+            <ul class="cta-list">
+              <li class="cta-item">
+                <i class="material-icons">&#xE8DC;</i>
+              </li>
+              <li class="cta-item">
+                <i class="material-icons">&#xE0B9;</i>
+              </li>
+              <li class="cta-item">
+                <i class="material-icons">&#xE80D;</i>
+              </li>
+              <li class="cta-item">
+                <i class="material-icons">&#xE5D4;</i>
+              </li>
+            </ul>
+          </div>
+        </div>
+        <div class="item05">
+          <img
+            src="http://geekhebdo.com/wp-content/uploads/2015/04/rs-compressed.jpg"
+            alt=""
+          />
+          <div class="meta">
+            <h2>Lorem ipsum dolor sit.</h2>
+            <ul class="cta-list">
+              <li class="cta-item">
+                <i class="material-icons">&#xE8DC;</i>
+              </li>
+              <li class="cta-item">
+                <i class="material-icons">&#xE0B9;</i>
+              </li>
+              <li class="cta-item">
+                <i class="material-icons">&#xE80D;</i>
+              </li>
+              <li class="cta-item">
+                <i class="material-icons">&#xE5D4;</i>
+              </li>
+            </ul>
+          </div>
+        </div>
+        <div class="item06">
+          <img src="https://pic.clubic.com/v1/images/1752458/raw" alt="" />
+          <div class="meta">
+            <h2>Lorem ipsum dolor sit.</h2>
+            <ul class="cta-list">
+              <li class="cta-item">
+                <i class="material-icons">&#xE8DC;</i>
+              </li>
+              <li class="cta-item">
+                <i class="material-icons">&#xE0B9;</i>
+              </li>
+              <li class="cta-item">
+                <i class="material-icons">&#xE80D;</i>
+              </li>
+              <li class="cta-item">
+                <i class="material-icons">&#xE5D4;</i>
+              </li>
+            </ul>
+          </div>
+        </div>
+        <div class="item07">
+          <img
+            src="https://static.lexpress.fr/medias_11518/w_1301,h_728,c_crop,x_0,y_169/w_480,h_270,c_fill,g_north/v1497362871/la-momie-d-alex-kurtzman_5897247.jpg"
+            alt=""
+          />
+          <div class="meta">
+            <h2>Lorem ipsum dolor sit.</h2>
+            <ul class="cta-list">
+              <li class="cta-item">
+                <i class="material-icons">&#xE8DC;</i>
+              </li>
+              <li class="cta-item">
+                <i class="material-icons">&#xE0B9;</i>
+              </li>
+              <li class="cta-item">
+                <i class="material-icons">&#xE80D;</i>
+              </li>
+              <li class="cta-item">
+                <i class="material-icons">&#xE5D4;</i>
+              </li>
+            </ul>
+          </div>
         </div>
       </div>
-      <div class="item02">
-        <img
-          src="https://tel.img.pmdstatic.net/fit/http.3A.2F.2Fprd2-bone-image.2Es3-website-eu-west-1.2Eamazonaws.2Ecom.2Ftel.2F2019.2F11.2F08.2F60ee5033-efd6-4c73-9403-a8fbaf4486cf.2Ejpeg/530x230/quality/80/slumdog-millionaire-arte-tournage-clandestin-casting-laborieux-la-genese-de-ce-film-phenomene.jpg"
-          alt=""
-        />
-        <div class="meta">
-          <h2>Lorem ipsum dolor sit.</h2>
-          <ul class="cta-list">
-            <li class="cta-item">
-              <i class="material-icons">&#xE8DC;</i>
-            </li>
-            <li class="cta-item">
-              <i class="material-icons">&#xE0B9;</i>
-            </li>
-            <li class="cta-item">
-              <i class="material-icons">&#xE80D;</i>
-            </li>
-            <li class="cta-item">
-              <i class="material-icons">&#xE5D4;</i>
-            </li>
-          </ul>
-        </div>
-      </div>
-      <div class="item03">
-        <img
-          src="https://upload.wikimedia.org/wikipedia/en/0/01/HarrietFilmPoster.jpeg"
-          alt=""
-        />
-        <div class="meta">
-          <h2>Lorem ipsum dolor sit.</h2>
-          <ul class="cta-list">
-            <li class="cta-item">
-              <i class="material-icons">&#xE8DC;</i>
-            </li>
-            <li class="cta-item">
-              <i class="material-icons">&#xE0B9;</i>
-            </li>
-            <li class="cta-item">
-              <i class="material-icons">&#xE80D;</i>
-            </li>
-            <li class="cta-item">
-              <i class="material-icons">&#xE5D4;</i>
-            </li>
-          </ul>
-        </div>
-      </div>
-      <div class="item04">
-        <img
-          src="https://www.lepoint.fr/images/2017/11/13/11277831lpw-11278045-article-jpg_4757541.jpg"
-          alt=""
-        />
-        <div class="meta">
-          <h2>Lorem ipsum dolor sit.</h2>
-          <ul class="cta-list">
-            <li class="cta-item">
-              <i class="material-icons">&#xE8DC;</i>
-            </li>
-            <li class="cta-item">
-              <i class="material-icons">&#xE0B9;</i>
-            </li>
-            <li class="cta-item">
-              <i class="material-icons">&#xE80D;</i>
-            </li>
-            <li class="cta-item">
-              <i class="material-icons">&#xE5D4;</i>
-            </li>
-          </ul>
-        </div>
-      </div>
-      <div class="item05">
-        <img
-          src="http://geekhebdo.com/wp-content/uploads/2015/04/rs-compressed.jpg"
-          alt=""
-        />
-        <div class="meta">
-          <h2>Lorem ipsum dolor sit.</h2>
-          <ul class="cta-list">
-            <li class="cta-item">
-              <i class="material-icons">&#xE8DC;</i>
-            </li>
-            <li class="cta-item">
-              <i class="material-icons">&#xE0B9;</i>
-            </li>
-            <li class="cta-item">
-              <i class="material-icons">&#xE80D;</i>
-            </li>
-            <li class="cta-item">
-              <i class="material-icons">&#xE5D4;</i>
-            </li>
-          </ul>
-        </div>
-      </div>
-      <div class="item06">
-        <img src="https://pic.clubic.com/v1/images/1752458/raw" alt="" />
-        <div class="meta">
-          <h2>Lorem ipsum dolor sit.</h2>
-          <ul class="cta-list">
-            <li class="cta-item">
-              <i class="material-icons">&#xE8DC;</i>
-            </li>
-            <li class="cta-item">
-              <i class="material-icons">&#xE0B9;</i>
-            </li>
-            <li class="cta-item">
-              <i class="material-icons">&#xE80D;</i>
-            </li>
-            <li class="cta-item">
-              <i class="material-icons">&#xE5D4;</i>
-            </li>
-          </ul>
-        </div>
-      </div>
-      <div class="item07">
-        <img
-          src="https://static.lexpress.fr/medias_11518/w_1301,h_728,c_crop,x_0,y_169/w_480,h_270,c_fill,g_north/v1497362871/la-momie-d-alex-kurtzman_5897247.jpg"
-          alt=""
-        />
-        <div class="meta">
-          <h2>Lorem ipsum dolor sit.</h2>
-          <ul class="cta-list">
-            <li class="cta-item">
-              <i class="material-icons">&#xE8DC;</i>
-            </li>
-            <li class="cta-item">
-              <i class="material-icons">&#xE0B9;</i>
-            </li>
-            <li class="cta-item">
-              <i class="material-icons">&#xE80D;</i>
-            </li>
-            <li class="cta-item">
-              <i class="material-icons">&#xE5D4;</i>
-            </li>
-          </ul>
-        </div>
       </div>
     </div>
   );
