58511d57bef2600f07c9d40ded491700b3ae75fb
FPaurisse <paurissefrederic@gmail.com>
2019-12-06 14:00:01 +0100

    delete WallPage into this branch

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
 import React from 'react';
 import './App.css';
-import WallPage from './components/WallPage/WallPage';
 import ParticipationPage from './components/ParticipationPage/ParticipationPage';
 
 function App() {
   return (
     <div className="App">
       <ParticipationPage />
-      {/* <WallPage /> */}
     </div>
   );
 }
