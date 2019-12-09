7b58c530875d86215a2aec2a02525efd82e3ca74
Adrien Nombalier <adri1n.apple@gmail.com>
2019-12-09 12:38:33 +0100

    add post

--- src/components/ParticipationForm/index.js
+++ src/components/ParticipationForm/index.js
@@ -1,9 +1,24 @@
 import React from 'react';
+import axios from 'axios';
 import ParticipantForm from './ParticipantForm';
 import QuestionsForm from './QuestionsForm';
 
 export default function Form() {
   const submitParticipation = () => {
+    axios({
+      method: 'post',
+      url: '/api/v1/questionnaires/1/participations',
+      data: [],
+    })
+      .then((reponse) => {
+        // On traite la suite une fois la réponse obtenue
+        console.log(reponse);
+      })
+      .catch((erreur) => {
+      // On traite ici les erreurs éventuellement survenues
+        console.log(erreur);
+      });
+
     localStorage.clear();
   };
 
