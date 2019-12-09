f79c6549463f0374e89d6a32ad518a9ae38d43bd
FPaurisse <paurissefrederic@gmail.com>
2019-12-09 16:19:06 +0100

    add inputs participant form

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
 import React from 'react';
 import './App.css';
 import ParticipationForm from './components/ParticipationForm';
-import ParticipantPage from './components/ParticipantPage/ParticipantPage';
 
 function App() {
   return (
     <div className="App">
       <ParticipationForm />
-      <ParticipantPage />
     </div>
   );
 }
--- src/components/ParticipationForm/ParticipantForm/index.js
+++ src/components/ParticipationForm/ParticipantForm/index.js
@@ -2,37 +2,75 @@ import React from 'react';
 import useLocalStorage from 'react-use-localstorage';
 
 export default function Participant({ addParticipant }) {
-  const [lastname, setName] = useLocalStorage('name', '');
+  const [firstName, setFirstName] = useLocalStorage('firstname', '');
+  const [lastName, setLastName] = useLocalStorage('lastname', '');
+  const [status, setStatus] = useLocalStorage('status', '');
   const [city, setCity] = useLocalStorage('city', '');
+  const [age, setAge] = useLocalStorage('age', '');
+  const [email, setEmail] = useLocalStorage('email', '');
 
-  const handleChangeName = (e) => {
-    setName(e.target.value);
+  const handleChangeFirstName = (e) => {
+    setFirstName(e.target.value);
+  };
+  const handleChangeLastName = (e) => {
+    setLastName(e.target.value);
+  };
+  const handleChangeStatus = (e) => {
+    setStatus(e.target.value);
   };
   const handleChangeCity = (e) => {
     setCity(e.target.value);
   };
+  const handleChangeAge = (e) => {
+    setAge(e.target.value);
+  };
+  const handleChangeEmail = (e) => {
+    setEmail(e.target.value);
+  };
 
   const displayQuestions = () => {
-    addParticipant({ lastname, city });
+    addParticipant({
+      firstName, lastName, city, status,
+    });
   };
 
   return (
     <div>
       <h2>Informations</h2>
       <input
-        placeholder="name"
-        onChange={handleChangeName}
-        value={lastname}
+        placeholder="firstname"
+        onChange={handleChangeFirstName}
+        value={firstName}
       />
+      <input
+        placeholder="lastname"
+        onChange={handleChangeLastName}
+        value={lastName}
+      />
+      <select onChange={handleChangeStatus}>
+        <option value="">--votre statut--</option>
+        <option value="teacher" selected={status === 'teacher' && 'selected'}>Enseignant</option>
+        <option value="student" selected={status === 'student' && 'selected'}>Élève</option>
+        <option value="other" selected={status === 'other' && 'selected'}>Autre</option>
+      </select>
       <input
         placeholder="City"
         onChange={handleChangeCity}
         value={city}
       />
+      <input
+        placeholder="Age"
+        onChange={handleChangeAge}
+        value={age}
+      />
+      <input
+        placeholder="Email"
+        onChange={handleChangeEmail}
+        value={email}
+      />
       <button
         type="button"
         onClick={displayQuestions}
-        disabled={!lastname && !city && 'disabled'}
       >
         Participer !
       </button>
