1286c294babac0a0bb059b282ab2fce51b91a5a1
Falcao1992 <eduardo.marselle@gmail.com>
2019-12-10 13:22:36 +0100

    localstorage en place

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
 import ParticipationForm from './components/ParticipationForm';
+import ParticipantPage from './components/ParticipantPage/ParticipantPage';
 
 function App() {
   return (
     <div className="App">
       <ParticipationForm />
+      <ParticipantPage />
     </div>
   );
 }
