b09bae0b74f387f01f86af18799b877a8b7517cc
Falcao1992 <eduardo.marselle@gmail.com>
2019-12-11 15:00:45 +0100

    architecture coté front done

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
 import ParticipationForm from './components/ParticipationForm';
+import UploadImage from './components/ParticipationForm/QuestionsForm/Question/UploadImage/UploadImage';
 
 function App() {
   return (
     <div className="App">
       <ParticipationForm />
+      <UploadImage />
     </div>
   );
 }
---
+++ src/components/ParticipationForm/QuestionsForm/Question/UploadImage/UploadImage.js
@@ -0,0 +1,39 @@
+import React, { Component } from 'react';
+
+class UploadImage extends Component {
+  constructor(props) {
+    super();
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <div className="container">
+        <h1>File Upload</h1>
+        {typeof msg !== 'undefined' ? msg : ''}
+        <form action="/upload" method="POST" encType="multipart/form-data">
+          <div className="file-field input-field">
+            <div className="btn grey">
+              <span>File</span>
+              <input name="myImage" type="file" />
+            </div>
+            <div className="file-path-wrapper">
+              <input className="file-path validate" type="text" />
+            </div>
+          </div>
+          <button type="submit" className="btn">
+            Submit
+          </button>
+        </form>
+        <br />
+        <img
+          src={typeof file !== 'undefined' ? file : ''}
+          className="responsive-img"
+          alt="prévisualisation"
+        />
+      </div>
+    );
+  }
+}
+
+export default UploadImage;
