b6cfdc0dcacc88b234fd4c432f2e95241aa546a1
FPaurisse <paurissefrederic@gmail.com>
2019-12-11 11:08:10 +0100

    rename variables

--- src/components/ParticipationForm/index.js
+++ src/components/ParticipationForm/index.js
@@ -4,22 +4,22 @@ import ParticipantForm from './ParticipantForm';
 import QuestionsForm from './QuestionsForm';
 
 export default function Form() {
-  const [dataParticipant, setDataParticipant] = useState(null);
+  const [participant, setParticipant] = useState(null);
   const [nbQuestions, setNbQuestions] = useState(0);
-  const [dataAnswers, setDataAnswers] = useState([]);
+  const [answers, setAnswers] = useState([]);
 
   const submitParticipation = (e) => {
     e.preventDefault();
-    axios.post('/api/v1/questionnaires/1/participations', { dataParticipant, dataAnswers });
+    axios.post('/api/v1/questionnaires/1/participations', { participant, answers });
     localStorage.clear();
   };
 
   return (
     <form onSubmit={submitParticipation}>
-      {!dataParticipant
-        ? <ParticipantForm addParticipant={setDataParticipant} />
-        : <QuestionsForm addAnswers={setDataAnswers} nbQuestions={setNbQuestions} />}
-      {dataAnswers.length === nbQuestions && nbQuestions > 0
+      {!participant
+        ? <ParticipantForm addParticipant={setParticipant} />
+        : <QuestionsForm addAnswers={setAnswers} nbQuestions={setNbQuestions} />}
+      {answers.length === nbQuestions && nbQuestions > 0
         && (
         <button type="submit">
           Send participation
