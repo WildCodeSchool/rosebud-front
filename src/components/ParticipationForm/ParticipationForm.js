98829a676c69af84711cd2c7716cd45bba74fe2a
FPaurisse <paurissefrederic@gmail.com>
2019-12-14 09:02:39 +0100

    add css

--- src/components/ParticipationForm/ParticipationForm.js
+++ src/components/ParticipationForm/ParticipationForm.js
@@ -1,8 +1,10 @@
 import React, { useState, useEffect } from 'react';
+import './ParticipationForm.css';
 import axios from 'axios';
 
 function ParticipationForm() {
   const [questions, setQuestions] = useState([]);
+  const [step, setStep] = useState(0);
 
   useEffect(() => {
     const fetchData = async () => {
@@ -12,56 +14,85 @@ function ParticipationForm() {
     fetchData();
   }, []);
 
-  const formSubmit = (e) => {
+  const submitParticipation = (e) => {
     e.preventDefault();
     const data = new FormData(e.target);
     axios.post('/api/v1/questionnaires/1/participations', data);
   };
 
+  const changeStep = (value) => {
+    setStep(step + value);
+  };
+
   return (
-    <form
-      action="/upload"
-      method="POST"
-      encType="multipart/formdata"
-      onSubmit={formSubmit}
-    >
-      <div className="step">
-        <label htmlFor="firstName">
-          <input name="firstName" type="text" placeholder="Prénom" />
-        </label>
-        <label htmlFor="lastname">
-          <input name="lastname" type="text" placeholder="Nom" />
-        </label>
-        <label htmlFor="status">
-          <input name="status" type="text" placeholder="Status" />
-        </label>
-        <label htmlFor="age">
-          <input name="age" type="text" placeholder="Age" />
-        </label>
-        <label htmlFor="city">
-          <input name="city" type="text" placeholder="Ville" />
-        </label>
-        <label htmlFor="email">
-          <input name="email" type="text" placeholder="E-mail" />
-        </label>
-      </div>
-      {
-        questions.map((question, index) => (
-          <div className="step" key={question.id}>
-            <h3>{question.title}</h3>
-            <label htmlFor={`answer-${index}-image`}>
-              <input name={`answer-${index}-image`} type="file" />
+    <>
+      {questions.length > 0
+        && (
+        <form
+          action="/upload"
+          method="POST"
+          encType="multipart/formdata"
+          onSubmit={submitParticipation}
+          className="ParticipationForm"
+        >
+          <div className={`${step < 1 ? 'step--show' : 'step--hide'}`}>
+            <label htmlFor="firstName">
+              <input name="firstName" type="text" placeholder="Prénom" />
             </label>
-            <label htmlFor={`answer-${index}-comment`}>
-              <textarea name={`answer-${index}-comment`} cols="30" rows="10" />
+            <label htmlFor="lastname">
+              <input name="lastname" type="text" placeholder="Nom" />
             </label>
+            <label htmlFor="status">
+              <select name="status" defaultValue="">
+                <option value="">Statut</option>
+                <option value="teacher">Enseignant</option>
+                <option value="student">Éléve</option>
+                <option value="other">autre</option>
+              </select>
+            </label>
+            <label htmlFor="age">
+              <input name="age" type="text" placeholder="Age" />
+            </label>
+            <label htmlFor="city">
+              <input name="city" type="text" placeholder="Ville" />
+            </label>
+            <label htmlFor="email">
+              <input name="email" type="text" placeholder="E-mail" />
+            </label>
+            <div className="pagination pagination--firststep">
+              <button type="button" onClick={() => changeStep(1)}>Participer</button>
+            </div>
           </div>
-        ))
-      }
-      <button type="submit">
-        Envoyer
-      </button>
-    </form>
+          {
+            questions.map((question, index) => (
+              <div className={`${step === index + 1 ? 'step--show' : 'step--hide'}`} key={question.id}>
+                <h3>{question.title}</h3>
+                <label htmlFor={`answer-${index}-image`}>
+                  <input name={`answer-${index}-image`} type="file" />
+                </label>
+                <label htmlFor={`answer-${index}-comment`}>
+                  <textarea name={`answer-${index}-comment`} cols="30" rows="10" />
+                </label>
+                {step < questions.length
+                  && (
+                    <div className="pagination">
+                      <button type="button" onClick={() => changeStep(-1)}>Précédent</button>
+                      <button type="button" onClick={() => changeStep(1)}>Suivant</button>
+                    </div>
+                  )}
+              </div>
+            ))
+          }
+          {step === questions.length
+            && (
+              <div className="pagination pagination--laststep">
+                <button type="button" onClick={() => changeStep(-1)}>Précédent</button>
+                <button type="submit">Valider ma participation</button>
+              </div>
+            )}
+        </form>
+        )}
+    </>
   );
 }
 
