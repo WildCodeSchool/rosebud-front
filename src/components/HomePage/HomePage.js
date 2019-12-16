d4120d4693f7831efbd86bbba76079fe9afcd917
FPaurisse <paurissefrederic@gmail.com>
2019-12-16 11:25:13 +0100

    add style and content HomePage

--- src/components/HomePage/HomePage.js
+++ src/components/HomePage/HomePage.js
@@ -5,8 +5,101 @@ import { Link } from 'react-router-dom';
 function HomePage() {
   return (
     <div className="HomePage">
-      <p>Home</p>
-      <Link to="/participation/questionnaire/1">Questionnaire 1</Link>
+
+      <div className="random__images__wrapper">
+        <img className="random__image image__1" src="http://lorempixel.com/640/360/" alt="alt" />
+        <img className="random__image image__2" src="http://lorempixel.com/641/361/" alt="alt" />
+        <img className="random__image image__3" src="http://lorempixel.com/642/362/" alt="alt" />
+        <img className="random__image image__4" src="http://lorempixel.com/643/363/" alt="alt" />
+        <img className="random__image image__5" src="http://lorempixel.com/644/364/" alt="alt" />
+        <div className="random__images__button__wrapper">
+          <button type="button" className="random__images__button">
+            <i className="random__images__button__icon fa fa-plus-square" />
+            <p className="random__images__button__content">Participer</p>
+          </button>
+        </div>
+      </div>
+
+      <section className="home__counters">
+        <div className="home__counters__title">
+            12
+          <span className="home__counters__length">thématiques</span>
+        </div>
+        <div className="home__counters__title">
+            142
+          <span className="home__counters__length">questionnaires</span>
+        </div>
+        <div className="home__counters__title">
+            463
+          <span className="home__counters__length">participants</span>
+        </div>
+      </section>
+
+      <section className="about__us">
+        <div className="about__us__wrapper">
+          <h2 className="about__us__title">
+            <i className="about__us__icon fa fa-info" />
+                À propos de Rosebud
+          </h2>
+          <p className="about__us__content">
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Exercitationem
+            {' '}
+            <strong>debitis</strong>
+            {' '}
+            commodi magnam soluta! In debitis, officiis, a nam
+            {' '}
+            <strong>repellat</strong>
+            {' '}
+            tenetur ex, vero dignissimos ad doloribus reiciendis? Eum eius qui vitae!
+          </p>
+        </div>
+      </section>
+
+      <section className="home__search">
+        <div className="home__search__input__wrapper">
+          <input type="text" placeholder="Rechercher..." className="home__search__input" />
+        </div>
+
+        <div className="search__results">
+          <div className="search__results__buttons">
+            <button type="button" className="search__results__button search__results__button--active">Consulter</button>
+            <button type="button" className="search__results__button">Participer</button>
+          </div>
+          <div className="search__results__wrapper">
+            <Link to="participation/questionnaire/1" className="search__results__item">
+              <div className="search__results__item__infos">
+                <h3 className="search__results__item__title">Classes pilotes Courts métrages / Jeu vidéo</h3>
+                <p className="search__results__item__content">Lorem ipsum dolor sit amet consectetur adipisicing sicing elit...</p>
+              </div>
+              <div className="search__results__access"><i className="fa fa-arrow-right" aria-hidden="true" /></div>
+            </Link>
+          </div>
+          <div className="search__results__pagination">
+            <span className="search__results__pagination__item search__results__pagination__item--active" />
+            <span className="search__results__pagination__item" />
+            <span className="search__results__pagination__item" />
+          </div>
+        </div>
+      </section>
+
+      <footer className="Footer">
+        <div className="Footer__wrapper">
+          <ul className="Footer__list">
+            <h2 className="Footer__title">Informations</h2>
+            <li className="Footer__list__item">Lorem ipsum</li>
+            <li className="Footer__list__item">Ipsumlorem lox</li>
+            <li className="Footer__list__item">Mentions Légales</li>
+          </ul>
+          <ul>
+            <li className="Footer__list__item"><i className="Footer__icon fa fa-facebook-square" /></li>
+            <li className="Footer__list__item"><i className="Footer__icon fa fa-twitter-square" /></li>
+            <li className="Footer__list__item"><i className="Footer__icon fa fa-instagram" /></li>
+          </ul>
+        </div>
+        <div className="Footer__copyright">
+            © 2020 - Ciclic
+        </div>
+      </footer>
     </div>
   );
 }
