e4f2d4abd4f9edcab00418c3469b8d30b7284fc6
François Boisselier <francois.boisselier@gmail.com>
2019-12-16 10:55:02 +0100

    no change

---
+++ src/components/WallPage/Wallpage.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Participation from './Participation/Participation'
+
+function Wallpage (props){
+    return(
+        <>
+            <Participation />
+        </>
+    )
+}
+
+
+export default Wallpage
