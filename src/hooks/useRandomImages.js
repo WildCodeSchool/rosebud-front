b33c7b9b0dd0fe947b508040fbb6f553021e1ad4
FPaurisse <paurissefrederic@gmail.com>
2020-02-01 17:21:59 +0100

    change uri random images

--- src/hooks/useRandomImages.js
+++ src/hooks/useRandomImages.js
@@ -6,7 +6,7 @@ const useRandomImages = (limit, QuestionnaireId) => {
 
   React.useEffect(() => {
     const fetchImages = async () => {
-      const result = await api.get(`/api/v1/questionnaires/${QuestionnaireId}?limit=${limit}`);
+      const result = await api.get(`/api/v1/questionnaires?limit=${limit}`);
       setImages(result.data);
     };
     fetchImages();
