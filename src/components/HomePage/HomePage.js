f1c8a28ca4ccf4542278449300d4f1c4282eedaf
FPaurisse <paurissefrederic@gmail.com>
2020-02-07 10:16:31 +0100

    change placeholder

--- src/components/HomePage/HomePage.js
+++ src/components/HomePage/HomePage.js
@@ -54,7 +54,7 @@ function HomePage() {
     }
     let endingText = ending;
     if (endingText !== null) {
-      endingText = '...';
+      endingText = '…';
     }
     if (str.length > lengthText) {
       return str.substring(0, lengthText - endingText.length) + endingText;
@@ -126,7 +126,7 @@ function HomePage() {
             <input
               type="text"
               value={querySearch || ''}
-              placeholder="Rechercher un questionnaire..."
+              placeholder="Rechercher un questionnaire…"
               className="home__search__input"
               onChange={(e) => {
                 setQuerySearch(e.target.value);
